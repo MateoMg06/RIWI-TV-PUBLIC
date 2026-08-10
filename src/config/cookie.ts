a2fdeabbf6bff38046b6fe697532a35c40713fc1
Kevin Bonifacio <kevindavid8947@hotmail.com>
2026-08-10 08:49:07 -0500

    fusion dos ramas

--- src/config/cookie.ts
+++
@@ -1,8 +0,0 @@
-const isProd: boolean= process.env.NODE_ENV=== "production"
-
-export const cookieOptions: object= {
-    httpOnly: true, // Cookie solo accesible por medio de peticiones HTTP, no JS
-    secure: isProd, // Si está en producción, solo es accesible por HTTPS
-    samesite: isProd ? "none" : "lax", // En producción se puede transportar entre diferentes dominios, sino, se limita al mismo (development)
-    maxAge: 60 * 60 * 1000 // 1 hora en milisegundos
-}
